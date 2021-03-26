637c92fc15294f04c2d2e099babcc1b4d13ffb4f
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:34:24 +0700

    Update baka.js

--- commands/baka.js
+++ commands/baka.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const superagent = require('superagent');
 module.exports = {
-    name: "spank",
+    name: "baka",
 
     async run(client, message, args) {
         message.delete();
