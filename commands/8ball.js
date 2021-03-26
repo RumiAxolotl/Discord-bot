e571690ba51ef00c26094fc69bf2d9ef82743e4c
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 23:46:29 +0700

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -13,14 +13,28 @@ module.exports = {
         "Yas",
         "Nope",
         "Definetly",
-        "Absoloutely",
         "Not in a million years",
         "Maybe or Maybe not",
         "Not have any hope",
         "It is decidedly so",
         "It is certain",
         "As i see it, YES"
-        "Can't be!"
+        "Can't be!",
+        " Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don’t count on it",
+        "Most likely",
+        "My reply is NO",
+        "My sources say NO",
+        "Outlook not so good",
+        "Outlook good",
+        "Reply hazy, try again",
+        "Signs point to YES",
+        "Very doubtful",
+        "Without a doubt",
+        "You may rely on it",
       ];
       let response =
         responses[Math.floor(Math.random() * responses.length - 1)];
