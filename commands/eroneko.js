6b95425d70e8274d567d8416d62dbf7b8d978d51
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:41:18 +0700

    Create eroneko.js

---
+++ commands/eroneko.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+module.exports = {
+    name: "eroneko",
+
+    async run(client, message, args) {
+        message.delete();
+        if (!message.channel.nsfw) {
+            message.react('💢');
+            return message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "You can use this command in an NSFW Channel!"
+                }
+            })
+        }
+        superagent.get('https://nekos.life/api/v2/img/erokemo')
+            .end((err, response) => {
+                const embed = new Discord.RichEmbed()
+                    .setTitle(":smirk: Eroneko")
+                    .setImage(response.body.url)
+                    .setColor(`RANDOM`)
+                    .setFooter(`Tags: eroneko`)
+                    .setURL(response.body.url);
+                message.channel.send(embed);
+            }).catch((err) => message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "Something went wrong... :cry:"
+                }
+            }));
+
+    }
+}
