05d533d7761ad1c58dde4bec8108473c645fb378
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 18:55:56 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,22 +7,23 @@ module.exports = {
 
     async run(client, message, args) {
         if (message.channel.type === 'dm') return;
-        let embed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
-            .setImage(message.guild.iconURL)
-            .addField(`${message.guild}'s informations`, stripIndents`**- Owner: ** ${message.guild.owner}
-            **- Location: **${message.guild.region}
-            **- Created At: **${message.guild.createdAt}
-            ---------------------------------
-            **+ Member Count: ** \`${message.guild.memberCount}\`
-            **+ Emoji Count: ** \`${message.guild.emojis.cache.size}\`
-            **+ Roles Count: ** \`${message.guild.roles.cache.size}\`
-            ---------------------------------
-            **- Tier Boots: ** ${message.guild.premiumTier}
-            **- Number of Boosts: **  ${message.guild.premiumSubscriptionCount}
-            `)
-            .setTimestamp()
-            .setThumbnail(`${message.guild.iconURL()}`);
-        message.channel.send(embed)
+        let embed = new MessageEmbed()
+            .setColor(cyan)
+            .setTitle("Server Info")
+            .setThumbnail(message.guild.iconURL())
+            .setAuthor(`${message.guild.name}`, message.guild.iconURL())
+            .addField("**Guild Owner:**", `${message.guild.owner}`, true)
+            .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+            .addField("**Total Real Members**", message.guild.members.cache.filter(member => !member.user.bot).size, true)
+            .addField("**Total Bots**", message.guild.members.cache.filter(member => member.user.bot).size, true)
+            .addField("**Total Channels**", message.guild.channels.cache.size, true)
+            .addField("**Total Text Channels**", message.guild.channels.cache.filter(ch => ch.type === 'text').size, true)
+            .addField("**Total Voice Channels**", message.guild.channels.cache.filter(ch => ch.type === 'voice').size, true)
+            .addField("**Created On**", message.guild.createdAt.toLocaleString(), true)
+            .setDescription(`${message.guild.roles.cache.map(role => role.toString()).join(' ')}`)
+            .setFooter(`${message.guild.me.displayName}`, client.user.displayAvatarURL())
+            .setTimestamp();
+
+        message.channel.send(embed);
     }
 }
