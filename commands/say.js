d624835eb7570286663c65ff423a7a1c4749eddc
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 19:24:40 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -12,11 +12,11 @@ module.exports = {
 
         if(textChannel) {
             msg = args.slice(1).join(" ");
-            const embed = new MessageEmbed().setFooter(`~~A Duck Apears And Says~~`).setColor('#fcd303').setDescription(`${msg}`).setTimestamp();
+            const embed = new MessageEmbed().setColor('#fcd303').addField(`**A Duck Apear And Say**`,`${msg}`, true).setTimestamp();
             textChannel.send(embed);
         } else {
             msg = args.join(" ");
-            const embed = new MessageEmbed().setFooter(`~~A Duck Apears And Says~~`).setColor('#fcd303').setDescription(`${msg}`).setTimestamp();
+            const embed = new MessageEmbed().setColor('#fcd303').addField(`**A Duck Apear And Say**`, `${msg}`, true).setTimestamp();
             message.channel.send(embed)
         }
     }
