bdcabbf478b952beef3c9e166325a0fa5c07ee60
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:09:21 +0700

    Update channelcount.js

--- commands/channelcount.js
+++ commands/channelcount.js
@@ -8,7 +8,12 @@ module.exports = {
     async run(client, message, args) {
         if (message.channel.type === 'dm') return;
         if (message.author.bot) return;
-        message.channel.send('I am Active in ' + '**`' + ` ${client.channels.cache.size} ` + '`**' + ' Channels');
-        console.log('Someone Executed The Check Channel Size Command(s)')
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")     
+        .setDescription("I'm in " + `${client.channels.cache.size}` + " servers!")
+        .setTimestamp()
+        .setFooter(message.author.username, message.author.avatarURL);
+    message.channel.send(embed);
+    console.log('Someone Executed The Check Channel Size Command(s)')
     }
 }
