fad46c86db4299ad4217e3d2089425bbfcb4bf13
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:49:50 +0700

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -7,7 +7,7 @@ module.exports = {
         message.delete();
         superagent.get('https://nekos.life/api/v2/img/woof')
             .end((err, response) => {
-                const embed = new Discord.RichEmbed()
+                const embed = new Discord.MessageEmbed()
                     .setTitle("Random dog")
                     .setImage(response.body.url)
                     .setColor("RANDOM")
