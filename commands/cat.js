7a2e561afef9e4c280c71a365870098c367d4adb
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:45:46 +0700

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+module.exports = {
+    name: "cat",
+
+    async run(client, message, args) {
+        message.delete();
+        superagent.get('https://nekos.life/api/v2/img/meow')
+            .end((err, response) => {
+                const embed = new Discord.RichEmbed()
+                    .setTitle("Random cat")
+                    .setImage(response.body.url)
+                    .setColor(`RANDOM`)
+                    .setFooter(`meow`)
+                    .setURL(response.body.url);
+                message.channel.send(embed);
+            }).catch((err) => message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "Something went wrong... :cry:"
+                }
+            }));
+
+    }
+}
