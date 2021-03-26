82ff24364ad4459c9681a9f346c83e7fc9d123e3
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 18:19:21 +0700

    Create offline.js

---
+++ commands/offline.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const { ownerID } = require("../config.json");
+module.exports = {
+    name: "offline",
+    description: "Set bot status to online",
+
+    async run(client, message, args) {
+        if (message.author.id != ownerID) return;
+        client.user.setPresence({
+            status: 'invisible',
+        });
+        message.delete();
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle(`**Offline**`)
+            .setDescription(`Bye, have a good time`)
+            .setTimestamp()
+            .setColor(`#3d3d3d`);
+        message.channel.send(embed).then((sent) => {
+            setTimeout(function () {
+                sent.delete();
+            }, 5000);
+        });
+    }
+}
