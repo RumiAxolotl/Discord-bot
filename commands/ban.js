4f6274c9b523149d2f15e1bcb7b11386a50364a2
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 19:21:41 +0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,43 +4,40 @@ module.exports = {
     name: "ban",
     description: "Bans a member from the server",
 
-    async run(client, message, args) {
-        //check if the member running the ban command has the permission to ban members
-        if (!message.member.hasPermission('BAN_MEMBERS')) {
-            message.channel.send("You do not have enoug powah to use this command!");
-        } else if (!args[0]) {
-            //check if there was no first argument
-            message.channel.send("You have to enter a user to ban.");
-        } else if (!args[1]) {
-            //check if there was no second argument
-            message.channel.send("Enter a ban reason.");
-        } else {
-            //attempt to ban the user
-            try {
-                const banned = await message.mentions.members.first(); //get the first member that was mentioned
-                const banner = message.author.tag; //get the user that sent the command
-                const reason = args[1]; //get the second argument
+    async run (client, message, args) {
+    if(message.channel.type === 'dm') return;
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that, Quack!')
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions, Quack!')
 
-                //if the banned user exists
-                if (banned) {
-                    //if the user is not bannable return a message to the channel
-                    if (!message.guild.member(banned).bannable) return message.channel.send("That user is not bannable.");
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-                    await banned.ban(); //ban the user
+        if(!args[0]) return message.channel.send('Please specify a user');
 
-                    //create an embed with the ban info and send it to the mod-logs channel
-                    const embed = new Discord.MessageEmbed()
-                        .setColor("RED")
-                        .setTitle(`Member banned by ${banner}`)
-                        .addField('Banned Member', `${banned}`, true)
-                        .addField('Server', `${message.guild.name}`, true)
-                        .setDescription(`**Reason:** ${reason}`)
-                        .setTimestamp();
-                    message.send(embed);
-                } else {
-                    message.channel.send("Member not found.");
-                }
-            }
-        }
+        if(!member) return message.channel.send('Can\'t  find this duck. Quack');
+        if(!member.bannable) return message.channel.send('This duck can\'t be banned. They\'re too powahfool, Quack! ');
+
+        if(member.id === message.author.id) return message.channel.send('Quack Quack, you can\'t ban yourself!');
+
+        let banReason = args.slice(1).join(" ");
+
+        if(!banReason) banReason = 'Unspecified';
+
+        member.ban({ reason: banReason })
+            .catch(err => {
+                if (err) return message.channel.send('Something went wrong')
+            });
+
+        const banembed = new Discord.MessageEmbed()
+        .setTitle('Member Banned')
+        .setThumbnail(member.user.displayAvatarURL())
+        .addField('User Banned', member, true)
+        .addField('Banned by', message.author, true)
+        .addField('Reason', banReason, true)
+        .setFooter('Time banned', client.user.displayAvatarURL())
+        .setTimestamp()
+
+        message.channel.send(banembed);
+
+
     }
 }
