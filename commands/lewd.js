4eafc3e2ece193cd90c0d1234437523476d4c1ac
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 20:47:40 +0700

    Create lewd.js

---
+++ commands/lewd.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+module.exports = {
+    name: "lewd",
+    description: "get lewd image",
+
+    async run(client, message, args) {
+        message.delete();
+        superagent.get('https://nekos.life/api/v2/img/lewd')
+            .end((err, response) => {
+                const embed = new Discord.MessageEmbed()
+                    .setTitle('Lewd Image')
+                    .setImage(response.body.url)
+                    .setColor(`RANDOM`)
+                    .setFooter(`oh mlem`)
+                    .setURL(response.body.url);
+                message.channel.send(embed);
+            }).catch((err) => message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "Something went wrong... :cry:"
+                }
+            }));
+    }
+}
