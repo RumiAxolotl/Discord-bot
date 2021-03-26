f7baaa21fb5f2c051a2f55e503f3d662b00d913b
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:16:09 +0700

    Create femdom.js

---
+++ commands/femdom.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+module.exports = {
+    name: "spank",
+
+    async run(client, message, args) {
+        message.delete();
+        if (!message.channel.nsfw) {
+            message.react('💢');
+            return message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "You can use this command in an NSFW Channel!"
+                }
+            })
+        }
+        superagent.get('https://nekos.life/api/v2/img/femdom')
+            .end((err, response) => {
+                const embed = new Discord.RichEmbed()
+                    .setTitle("Femdom")
+                    .setImage(response.body.url)
+                    .setColor(`RANDOM`)
+                    .setFooter(`Tags: femdom`)
+                    .setURL(response.body.url);
+                message.channel.send(embed);
+            }).catch((err) => message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "Something went wrong... :cry:"
+                }
+            }));
+    }
+}
