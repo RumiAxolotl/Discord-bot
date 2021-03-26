05f7e7fa5fcaaafd7fb5f73f35051e3b771a7d52
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:33:23 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
             .addField(`**Moderation:**`, "`kick`, `ban`, `mute`, `unmute`, `slowmode`, `clear`")
             .addField(`**Fun:**`, "`kick`, `hug`, `pat`, `slap`, `tickle`, `baka`, `poke`, `8ball`")
             .addField(`**Util:**`, "`ping`, `serverinfo`, `userinfo`, `channelcount`,`servercount`, `weather`, `poll`, `say`")
-            .addField(`**NSFW**:`, "`nsfw`, `cum`, `spank`, `femdom`")
+            .addField(`**NSFW**:`, "`nsfw`, `cum`, `spank`, `femdom`, `foxgirl`")
             .addField(`**Other:**`, "`cfs`, `calculate`")
             .setFooter("Get the link with d*invite command!")
             .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`);
