885d19897ace0295570b8888897db32830cf536a
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:49:52 +0700

    Update erofeet.js

--- commands/erofeet.js
+++ commands/erofeet.js
@@ -16,7 +16,7 @@ module.exports = {
         }
         superagent.get('https://nekos.life/api/v2/img/erofeet')
             .end((err, response) => {
-                const embed = new Discord.RichEmbed()
+                const embed = new Discord.MessageEmbed()
                     .setTitle(":smirk: Erofeet")
                     .setImage(response.body.url)
                     .setColor(`RANDOM`)
