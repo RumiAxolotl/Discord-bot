d879c091865d0bfd38ad1c6f2393ec85672c4844
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 16:01:10 +0700

    Create dnd.js

---
+++ commands/dnd.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "dnd",
+    description: "Set bot status to online",
+
+    async run(client, message, args) {
+        if (!message.member.permissions.has("ADMINISTRATOR"))
+            return message.channel.send(
+                `You don't have enough powah to use this command, ${message.author.username}`)
+        client.user.setPresence({
+            status: 'Do not disturb',
+        });
+        message.delete();
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle(`**Do Not Disturb**`)
+            .setDescription(`I'm Busy`)
+            .setTimestamp()
+            .setColor(`#ff0000`);
+        message.channel.send(embed).then((sent) => {
+            setTimeout(function () {
+                sent.delete();
+            }, 5000);
+        });
+    }
+}
