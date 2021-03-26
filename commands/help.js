1e8e078af168f5211ac432f1e6f049bf16c9e032
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 23:16:24 +0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "help",
+    description: "show commands of the bot",
+
+    async run(client, message, args) {
+        message.delete();
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle('HELP')
+            .setTimestamp()
+            .setColor('#fcd303')
+            .addField(`**Moderation:**`, `kick; ban; mute; unmute; slowmode`)
+            .addField(`**Fun:**`, `kick; hug; pat; slap; spank; tickle; poke; 8ball`)
+            .addField(`**Util:**`, `ping; serverinfo; userinfo; channelcount; weather`)
+            .addField(`**Other:**`, `nsfw; cfs; calculate; poll; say`)
+            .setFooter(`Get the link with \`invite\` command!`)
+            .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`);
+        message.channel.send(embed);
+    }
+}
