98c63a23556d11a26f89cfcad998e2cbb12bed61
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 18:50:24 +0700

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -22,7 +22,7 @@ module.exports = {
                     .setColor("RANDOM")
                     .setTitle("User Info")
                     .setThumbnail(member.user.displayAvatarURL())
-                    .setAuthor(`${member.user.tag} (${member.id})`, member.user.displayAvatarURL())
+                    .setFooter(`${member.user.tag} `, member.user.displayAvatarURL())
                     .addField("**Username:**", `${member.user.username}`, true)
                     .addField("**Discriminator:**", `#${member.user.discriminator}`, true)
                     .addField("**ID:**", `${member.user.id}`, true)
