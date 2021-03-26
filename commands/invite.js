9303a9e3ecfebfa15c3428222f1fdf06a9965ad0
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 15:40:21 +0700

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "invite",
+    description: "Get the link to invite Ducky bot",
+
+    async run(client, message, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle(`**Invite`)
+            .setColor(`#fcd303`)
+            .setDescription(`You can invite me with the link:\nhttps://discord.com/api/oauth2/authorize?client_id=750718927985967144&permissions=8&scope=bot`)
+            .setTimestamp()
+            .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`);
+    }
+}
