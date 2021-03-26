03ad0cf63f712e98cf129f9da93e386e573c0ed6
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 19:07:34 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { stripIndents } = require("common-tags");
 
 module.exports = {
     name: "serverinfo",
@@ -7,22 +8,21 @@ module.exports = {
     async run(client, message, args) {
         if (message.channel.type === 'dm') return;
         let embed = new Discord.MessageEmbed()
-            .setColor(cyan)
+            .setColor("RANDOM")
+            .setImage(message.guild.iconURL)
             .setTitle("Server Info")
-            .setThumbnail(message.guild.iconURL())
-            .setAuthor(`${message.guild.name}`, message.guild.iconURL())
-            .addField("**Guild Owner:**", `${message.guild.owner}`, true)
+            .setTimestamp()
+            .setThumbnail(`${message.guild.iconURL()}`)
+            .addField("**Owner:**", `${message.guild.owner}`, true)
+            .addField("**Location:**", `${message.guild.region}`, true)
+            .addField("**Created At:**", `${message.guild.createdAt}`, true)
             .addField("**Member Count:**", `${message.guild.memberCount}`, true)
-            .addField("**Total Real Members**", message.guild.members.cache.filter(member => !member.user.bot).size, true)
-            .addField("**Total Bots**", message.guild.members.cache.filter(member => member.user.bot).size, true)
-            .addField("**Total Channels**", message.guild.channels.cache.size, true)
-            .addField("**Total Text Channels**", message.guild.channels.cache.filter(ch => ch.type === 'text').size, true)
-            .addField("**Total Voice Channels**", message.guild.channels.cache.filter(ch => ch.type === 'voice').size, true)
-            .addField("**Created On**", message.guild.createdAt.toLocaleString(), true)
-            .setDescription(`${message.guild.roles.cache.map(role => role.toString()).join(' ')}`)
-            .setFooter(`${message.guild.me.displayName}`, client.user.displayAvatarURL())
-            .setTimestamp();
-
-        message.channel.send(embed);
+            .addField("**Emoji Count:**", `${message.guild.emojis.cache.size}`, true)
+            .addField("**Roles Count:**", `${message.guild.roles.cache.size}`, true)
+            .addField("**Tier Boots:**", `${message.guild.premiumTier}`, true)
+            .addField("**Number of Boosts:**", `${message.guild.premiumSubscriptionCount}`, true)
+            .setTimestamp()
+            .setThumbnail(`${message.guild.iconURL()}`);
+        message.channel.send(embed)
     }
 }
