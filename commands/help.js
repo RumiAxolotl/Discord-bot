41563250961b4e8aa7789210566528ac5d77cdb4
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 23:17:42 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
             .setColor('#fcd303')
             .addField(`**Moderation:**`, `kick; ban; mute; unmute; slowmode`)
             .addField(`**Fun:**`, `kick; hug; pat; slap; spank; tickle; poke; 8ball`)
-            .addField(`**Util:**`, `ping; serverinfo; userinfo; channelcount; weather`)
+            .addField(`**Misc:**`, `ping; serverinfo; userinfo; channelcount; weather`)
             .addField(`**Other:**`, `nsfw; cfs; calculate; poll; say`)
             .setFooter(`Get the link with \`invite\` command!`)
             .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`);
