94fbc990c3593152d3ec7a98bea854e0635c4f2c
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-27 00:03:24 +0700

    Update spank.js

--- commands/spank.js
+++ commands/spank.js
@@ -2,34 +2,22 @@ const Discord = require('discord.js');
 const superagent = require('superagent');
 module.exports = {
     name: "spank",
-    description: "spank someone",
 
     async run(client, message, args) {
         message.delete();
-        const user = message.mentions.users.first();
-        if (!user)
-            return message.channel.send({
-                embed: {
-                    color: 16734039,
-                    description: "You must mention someone to spank!"
-                }
-            })
-        if (message.author === user) {
-            return await message.channel.send({
-                embed: {
-                    color: 16734039,
-                    description: "You cant spank yourself!"
-                }
-            })
-        }
+        if (!message.channel.nsfw) return message.channel.send({
+            embed: {
+                color: 16734039,
+                description: "You can use this command in an NSFW Channel!"
+            }
+        })
+
         superagent.get('https://nekos.life/api/v2/img/spank')
             .end((err, response) => {
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(user.username + " Just got spank by " + message.author.username)
+                const embed = new Discord.RichEmbed()
+                    .setTitle(":smirk: Spank")
                     .setImage(response.body.url)
-                    .setColor("RANDOM")
-                    .setDescription((user.toString() + " got spank by " + message.author.toString()))
-                    .setFooter(`bruh`)
+                    .setColor(`RANDOM`)
                     .setURL(response.body.url);
                 message.channel.send(embed);
             }).catch((err) => message.channel.send({
