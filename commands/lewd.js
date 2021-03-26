e4ba51f9ab19629fcc1728d3681210ac2d191382
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-26 22:06:25 +0700

    Update lewd.js

--- commands/lewd.js
+++ commands/lewd.js
@@ -6,19 +6,22 @@ module.exports = {
 
     async run(client, message, args) {
         message.delete();
-        superagent.get('https://nekos.life/api/v2/img/lewd')
-            .end((err, response) => {
-                const embed = new Discord.MessageEmbed()
-                    .setTitle('Lewd Image')
-                    .setImage(response.body.url)
-                    .setColor(`RANDOM`)
-                    .setURL(response.body.url);
-                message.channel.send(embed);
-            }).catch((err) => message.channel.send({
-                embed: {
-                    color: 16734039,
-                    description: "Something went wrong... :cry:"
-                }
-            }));
+        if (!message.channel.NSFW) return message.channel.send('**NSFW Channel only!**')
+        else {
+            superagent.get('https://nekos.life/api/v2/img/lewd')
+                .end((err, response) => {
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle('Lewd Image')
+                        .setImage(response.body.url)
+                        .setColor(`RANDOM`)
+                        .setURL(response.body.url);
+                    message.channel.send(embed);
+                }).catch((err) => message.channel.send({
+                    embed: {
+                        color: 16734039,
+                        description: "Something went wrong... :cry:"
+                    }
+                }));
+        }
     }
 }
