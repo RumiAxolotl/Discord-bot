083176fb93476098d9e07b2948659f39cf9aac1a
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-28 17:01:09 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,32 +4,20 @@ module.exports = {
     name: "avatar",
     description: "Brodcast someone's avatar",
 
-    async run(client, message, args) {
-        if (message.channel.type === 'dm') return;
-        if (!message.mentions.users.size) {
-            let embed = new Discord.MessageEmbed()
-		
-                .setColor("RANDOM")
-                .setAuthor(message.author.username + "'s Avatar", message.author.displayAvatarURL)
-                .setImage(message.author.displayAvatarURL);
-        
-            message.channel.send(embed)
-        }
+    async run (client, message, args) {
+    if(message.channel.type === 'dm') return;
+        let member = message.mentions.users.first() || message.author
 
-        const avatarList = message.mentions.users.map(user => {
-            return `${user.username},${user.displayAvatarURL}`;
-        });
+        let avatar = member.displayAvatarURL({size: 1024})
 
-        for (var i = 0; i < avatarList.length; i++) {
-            let Username = avatarList[i].split(',')[0];
-            let AvatarURL = avatarList[i].split(",").pop();
 
-            let embed = new Discord.MessageEmbed()
-                .setColor("RANDOM")
-                .setAuthor(Username + "'s Avatar", AvatarURL)
-                .setImage(AvatarURL);
-        
-            message.channel.send(embed);
-        }
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`${member.username}'s avatar`)
+        .setURL(avatar)
+        .setImage(avatar)
+        .setColor("RANDOM")
+        .setTimestamp()
+
+        message.channel.send(embed);
     }
 }
