3546a5f8a0b6e9536bf99fd467d3abda7a7ef897
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-28 16:15:14 +0700

    Create AI.js

---
+++ commands/AI.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const Discord = require('discord.js');
+module.exports = {
+    name: "AI",
+    description: "smart bot",
+
+    async run(client, message, args) {
+        message.delete();
+        try {
+            const res = await axios.get(`
+            http://api.brainshop.ai/get?bid=155428&key=aE6dItjROvW72dv8&uid=1&msg=${encodeURIComponent(message.content)}`);
+            message.channel.send(res.data.cnt);
+        }
+        catch (err) {
+            message.channel.send({
+                embed: {
+                    color: 16734039,
+                    description: "Bot Is ERROR, Try Again Later!"
+                }
+            }
+            );
+        }
+    }
+}
