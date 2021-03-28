eb7fe97fa1ef0b92ccb55097b6d2074096e75194
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-28 17:00:01 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,32 +4,32 @@ module.exports = {
     name: "avatar",
     description: "Brodcast someone's avatar",
 
-    async run (client, message, args) {
-    if(message.channel.type === 'dm') return;
-            if (!message.mentions.users.size) {     
-        let embed = new Discord.MessageEmbed()
+    async run(client, message, args) {
+        if (message.channel.type === 'dm') return;
+        if (!message.mentions.users.size) {
+            let embed = new Discord.MessageEmbed()
 		
-          .setColor("RANDOM")
-          .setAuthor(message.author.username + "'s Avatar", message.author.displayAvatarURL)
-          .setImage(message.author.displayAvatarURL)  
+                .setColor("RANDOM")
+                .setAuthor(message.author.username + "'s Avatar", message.author.displayAvatarURL)
+                .setImage(message.author.displayAvatarURL);
         
-        message.channel.send(embed)
-    }
+            message.channel.send(embed)
+        }
 
-    const avatarList = message.mentions.users.map(user => {
-        return `${user.username},${user.displayAvatarURL}`;
-    });
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username},${user.displayAvatarURL}`;
+        });
 
-    for (var i = 0; i < avatarList.length; i++) {
-        let Username = avatarList[i].split(',')[0];
-        let AvatarURL = avatarList[i].split(",").pop();
+        for (var i = 0; i < avatarList.length; i++) {
+            let Username = avatarList[i].split(',')[0];
+            let AvatarURL = avatarList[i].split(",").pop();
 
-        let embed = new Discord.RichEmbed()
-          .setColor("RANDOM")
-          .setAuthor(Username + "'s Avatar", AvatarURL)
-          .setImage(AvatarURL)
+            let embed = new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setAuthor(Username + "'s Avatar", AvatarURL)
+                .setImage(AvatarURL);
         
-        message.channel.send(embed);
+            message.channel.send(embed);
+        }
     }
 }
-    }
