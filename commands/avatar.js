60ae1b809d3e8c752d023ccf67e6c5064b99901c
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-28 17:07:22 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,23 +5,19 @@ module.exports = {
     description: "Brodcast someone's avatar",
 
     async run (client, message, args) {
-    const embed = new Discord.MessageEmbed()
     if(message.channel.type === 'dm') return;
-        if (!message.mentions.users.size) {
-            embed.setTitle(`${message.author.username}'s avatar`)
-                .setURL(message.author.displayAvatarURL)
-                .setImage(message.author.displayAvatarURL)
-                .setColor("RANDOM")
-                .setTimestamp();
-        }
+        let member = message.mentions.users.first() || message.author
 
-        const avatarList = message.mentions.users.map(user => {
-            embed.setTitle(`${user.username}'s avatar`)
-                .setURL(user.displayAvatarURL)
-                .setImage(user.displayAvatarURL)
-                .setColor("RANDOM")
-                .setTimestamp();
-        });
+        let avatar = member.displayAvatarURL({size: 1024})
+
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`${member.username}'s avatar`)
+        .setURL(avatar)
+        .setImage(avatar)
+        .setColor("RANDOM")
+        .setTimestamp()
+
         message.channel.send(embed);
     }
 }
