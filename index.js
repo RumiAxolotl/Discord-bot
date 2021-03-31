0481048749d88cb3601cf3f4f52ddc5258dead66
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-31 20:48:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const { token, default_prefix } = require('./config.json');
+const { token } = require('./config.json');
 
 const fs = require('fs');
 
@@ -10,9 +10,9 @@ const { join } = require('path');
 
 const config = require('./config.json');
 
-const axios = require('axios');
 
-let prefix = (config.default_prefix);
+
+
 
 
 client.commands = new Discord.Collection();
@@ -37,50 +37,6 @@ for (const file of eventFiles) {
 
 
 client.on("error", console.error);
-
-client.on("message", async message => {
-
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) {
-        if (message.channel.id == '825792822254764104') {
-            try {
-                const res = await axios.get(`http://api.brainshop.ai/get?bid=155428&key=aE6dItjROvW72dv8&uid=1&msg=${encodeURIComponent(message.content)}`);
-                const embed = new Discord.MessageEmbed();
-                embed.setTitle(`**AI Ducky**`)
-                    .setColor("RANDOM")
-                    .setDescription(res.data.cnt)
-                    .setFooter(`Powah by Axolotl#9352`)
-                    .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`)
-                    .setTimestamp();
-                message.channel.send(embed);
-            }
-            catch (e) {
-                message.channel.send({
-                    embed: {
-                        color: 16734039,
-                        description: "Bot Is ERROR, Try Again Later!"
-                    }
-                }
-                ).then((sent) => {
-                    setTimeout(function () {
-                        sent.delete();
-                    }, 2500);
-                });
-            }
-        }
-        else return;
-    }
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if (!client.commands.has(command)) return;
-    try {
-        client.commands.get(command).run(client, message, args);
-
-    } catch (error) {
-        console.error(error);
-    }
-})
-
 
 
 client.login(config.token);
