1606569b72108cb06cf8110e2934b5a6379ab7bd
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-31 22:20:45 +0700

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -2,9 +2,8 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "snipe",
     description: "Get a snipe of your choice in the channel!",
-    usage: "[snipe number]",
-    category: "fun",
-    run: async (client, message, args) => {
+
+    async run (client, message, args) {
         const snipes = client.snipes.get(message.channel.id) || [];
         const msg = snipes[args[0] - 1 || 0];
         if (!msg) return message.channel.send(`That is not a valid snipe...`);
