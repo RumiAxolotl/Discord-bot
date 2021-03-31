0ce770746b792d4ef5410ed3f5dd4b3910a91a2a
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-31 21:12:16 +0700

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -7,7 +7,6 @@ module.exports = {
     async run(client, message, args) {
         const msg = client.snipes.get(message.channel.id)
         const embed = new Discord.MessageEmbed()
-            .setAuthor(msg.author, msg.member.user.displayAvatarURL())
             .setDescription(msg.content)
             .setFooter('Get Sniped lol')
             .setTimestamp();
