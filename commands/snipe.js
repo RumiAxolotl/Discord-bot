364da8d0bfeba15b8babf1452b7d8ab4262d5920
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-31 21:06:54 +0700

    Create snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "snipe",
+    description: "get latest delete message",
+
+    async run(client, message, args) {
+        const msg = bot.snipes.get(message.channel.id)
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(msg.author, msg.member.user.displayAvatarURL())
+            .setDescription(msg.content)
+            .setFooter('Get Sniped lol')
+            .setTimestamp();
+        message.channel.send(embed);
+     }
+}
