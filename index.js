e5964acd73502501b1125337a1038c593d304adc
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-31 20:18:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
     const eventName = file.split(".")[0];
-    client.logger.event(`Loading EVT - ${eventName}`);
+    client.on(eventName, event.bind(null, client));
 }
 
 
