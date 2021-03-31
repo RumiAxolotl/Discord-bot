4f36932674f1b0c73258f2842eb131277ceb012f
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-03-31 21:15:43 +0700

    Update MessageDelete.js

--- events/MessageDelete.js
+++ events/MessageDelete.js
@@ -4,10 +4,10 @@ module.exports = {
     execute(client, message) {
         if (message.author.bot) return;
         client.snipes.set(message.channel.id, {
-            content: message.content,
-            author: message.author.tag,
-            member: message.member,
-            image: message.attachments.first() ? message.attachments.first().proxyURL : null
+            content: `${message.content}`,
+            author: `${message.author.tag}`,
+            member: `${message.member}`,
+            image: `${message.attachments.first() ? message.attachments.first().proxyURL : null}`
         })
     }
 }
