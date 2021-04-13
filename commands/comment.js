ebfa74290d3aed9ac9f03373d0d3992b9ba00dc6
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-04-14 05:28:33 +0700

    Update comment.js

--- commands/comment.js
+++ commands/comment.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
-const { ownerID } = require("../config.json");
+const config = require("../config.json");
 module.exports = {
     name: "comment",
     description: "receive comments about bot and code",
     async run(client, message, args) {
-        let owner = message.guild.members.get(ownerID);
+        let owner = message.guild.members.get(config.ownerID);
         if (message.author.bot) return;
         const embed = new Discord.MessageEmbed();
         embed.setAuthor(`${message.author.id}`)
