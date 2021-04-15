a4396d1dc190db6010e5bfbe1fbf54556f21432f
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-04-15 10:26:22 +0700

    Create snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: "snipe",
+    description: "A role utility command",
+    async run(client, message, args) {
+        const msg = client.snipes.get(message.channel.id)
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(msg.author, msg.member.user.displayAvatarURL())
+            .setDescription(msg.content)
+            .setFooter('Get Sniped lol')
+            .setTimestamp();
+        message.channel.send(embed);
+    }
+}
