b9b5561a8902b6099391a5a9d323d6707c4a5d34
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-04-15 10:35:20 +0700

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -4,6 +4,12 @@ module.exports = {
     description: "A role utility command",
     async run(client, message, args) {
         const msg = client.snipes.get(message.channel.id)
+        if (!msg) return await message.channel.send({
+            embed: {
+                color: 16734039,
+                description: "You cant slap yourself!"
+            }
+        })
         const embed = new Discord.MessageEmbed()
             .setDescription(msg.content)
             .setFooter('Get Sniped lol')
