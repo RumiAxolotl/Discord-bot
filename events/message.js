ee5e76432c2c7f2e5cc3ad53b49f0533d085423c
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-04-21 00:05:45 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'message',
     execute(message) {
+        if (message.author.bot) return;
         if (message.content.includes("<@!443728905908649985"))
             return message.reply("Don't mention my master, please send message with `d*comment + <content>` and my master will reply you!");
     }
