1a493dfa3add227e5a4e2e20dad127637f8555a6
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-04-20 23:58:35 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'message',
-    once: true,
     execute(message) {
-        if (message.content.includes(Axo)) return message.channel.send("Ông trùm Coder, Thiên tài tin học, Hacker bẩm sinh, Vua của bot")
+        if (message.content.includes("Axo")) return message.channel.send("Ông trùm Coder, Thiên tài tin học, Hacker bẩm sinh, Vua của bot")
     }
 }
