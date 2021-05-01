96f9735a6dd175b2fcaf6e34c8243f61a7931eab
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-05-02 03:18:24 +0700

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js')
 
-module.exports.run = async (client, message) => {
-    const msg = client.snipes.get(message.channel.id)
-    const embed = new Discord.MessageEmbed()
-        .setAuthor(msg.author, msg.member.user.displayAvatarURL())
-        .setDescription(msg.content)
-        .setFooter('Get Sniped lol')
-        .setTimestamp();
-    message.channel.send(embed);
-}
-module.exports.config = {
+module.exports = {
     name: "snipe",
-    description: "GOT SNIPE",
+    description: "Got snipe",
+    execute(client, message){
+        const msg = client.snipes.get(message.channel.id)
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(msg.author, msg.member.user.displayAvatarURL())
+            .setDescription(msg.content)
+            .setFooter('Get Sniped lol')
+            .setTimestamp();
+        message.channel.send(embed);
+    }
 }
