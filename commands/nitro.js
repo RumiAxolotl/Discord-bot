4e381de168ace028c1b3a3b4f05714d17497f3b6
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-05-20 18:07:58 +0700

    Update nitro.js

--- commands/nitro.js
+++ commands/nitro.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "nitro",
     description: "Free nitro gift!",
     async run(client,message) {
-        message.channel.send("https://dis.cord.gifts/4XYL5zBJq5j299al");
+        message.channel.send("https://discordgift.site/dzTGOnJGReZKFhBW");
     }
 }
