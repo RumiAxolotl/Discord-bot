2b2781210263982f140f5638bb0b9cee8b8589bc
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-05-20 18:06:18 +0700

    Update nitro.js

--- commands/nitro.js
+++ commands/nitro.js
@@ -1,9 +1,7 @@
-const Discord = require('discord.js');
-const { ownerID } = require("../config.json");
 module.exports = {
     name: "nitro",
     description: "Free nitro gift!",
-    async run(message) {
+    async run(client,message) {
         message.channel.send("https://dis.cord.gifts/4XYL5zBJq5j299al");
     }
 }
