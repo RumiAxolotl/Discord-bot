2c5a23f95b8b33af1b8f0048ca0a2c19a6a99d9d
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-08-03 13:37:24 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,5 +4,7 @@ module.exports = {
         if (message.author.bot) return;
         if (message.content.includes("<@!443728905908649985"))
             return message.reply("Don't mention my master, please send message with `d*feedback + <content>` and my master will reply you!");
+        if(message.content.startsWith("Axo"))
+            return message.channel.send("Axolotl? Never heard before!")
     }
 }
