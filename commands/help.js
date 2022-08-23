75d742f3da9168188f098e39e757d0dea1a1c70e
RumiAxolotl <rumiaxolotl@gmail.com>
2022-08-23 21:27:42 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,12 +9,25 @@ module.exports = {
             .setTitle('HELP')
             .setTimestamp()
             .setColor('#fcd303')
-            .addField(`**Moderation:**`, "`kick`, `ban`, `mute`, `unmute`, `clear`")
-            .addField(`**Fun:**`, "`kiss`, `hug`, `pat`, `slap`, `8ball`, `dog`, `cat`")
-            .addField(`**Util:**`, "`ping`, `serverinfo`, `userinfo`, `channelcount`,`servercount`, `weather`, `say`")
-            .addField(`**Other:**`, "`calculate`")
-            .setFooter("Get the link with d*invite command!")
+            .addFields({
+                name: `**Moderation:**`,
+                value: "`kick`, `ban`, `mute`, `unmute`, `clear`"
+            }, {
+                name: `**Fun:**`,
+                value: "`kiss`, `hug`, `pat`, `slap`, `8ball`, `dog`, `cat`"
+            }, {
+                name: `**Util:**`,
+                value: "`ping`, `serverinfo`, `userinfo`, `channelcount`,`servercount`, `weather`, `say`"
+            }, {
+                name: `**Other:**`,
+                value: "`calculate`"
+            })
+            .setFooter({
+                text: "Get the link with d*invite command!"
+            })
             .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`);
-        message.channel.send({ embeds: [embed] });
+        message.channel.send({
+            embeds: [embed]
+        });
     }
 }
