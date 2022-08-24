30834c22dbaff8096b8387565dcffbb73c71abd1
RumiAxolotl <rumiaxolotl@gmail.com>
2022-08-24 10:59:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,34 @@ client.on("error", console.error);
 
 client.on('messageCreate', async message => {
     if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) {
+        if (message.channel.id == '825682437861539850') {
+            try {
+                const res = await axios.get(`http://api.brainshop.ai/get?bid=155428&key=aE6dItjROvW72dv8&uid=1&msg=${encodeURIComponent(message.content)}`);
+                const embed = new Discord.MessageEmbed();
+                embed.setTitle(`**AI Ducky**`)
+                    .setColor("RANDOM")
+                    .setDescription(res.data.cnt)
+                    .setFooter({
+                        text: `Powah by Axolotl#9352`
+                    })
+                    .setThumbnail(`https://cdn.discordapp.com/icons/754326147566796811/148897d1d39622e4639cafa03b3d123a.png?size=4096`)
+                    .setTimestamp();
+                message.channel.send(embed);
+            } catch (e) {
+                message.channel.send({
+                    embed: {
+                        color: 16734039,
+                        description: "Bot Is ERROR, Try Again Later!"
+                    }
+                }).then((sent) => {
+                    setTimeout(function () {
+                        sent.delete();
+                    }, 2500);
+                });
+            }
+        } else return;
+    }
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
@@ -62,6 +89,15 @@ client.on('messageCreate', async message => {
 
     }
 });
+
+
+
+
+
+
+
+
+
 
 let announces = [
     "Nhớ uống nước đầy đủ nhé <3",
@@ -91,8 +127,7 @@ let task = new cron.CronJob('00 0,30 7-22 * * *', () => {
     member2.send({
         embeds: [Embed]
     })
-}  
-)
+})
 
 task.start();
 
