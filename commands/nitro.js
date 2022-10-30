60456cf84e8fde6cdb798ac2f150545e966bcb9b
RumiAxolotl <rumiaxolotl@gmail.com>
2022-10-31 01:18:39 +0700

    Delete nitro.js

--- commands/nitro.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "nitro",
-    description: "Free nitro gift!",
-    async run(client, message) {
-        message.channel.send("https://discordgift.site/dzTGOnJGReZKFhBW");
-    }
-}
