fda09151fe4db57b8cab48d80e080be3383ddcbb
RumiAxolotl <rumiaxolotl@gmail.com>
2024-07-19 18:10:59 +0700

    init new update version

--- Procfile
+++
@@ -1,1 +0,0 @@
-worker1: node index.js
--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        console.log(`${client.user.username} is ready, Quack`);
-        client.user.setActivity(`Rubber Duck`)
-    }
-}
