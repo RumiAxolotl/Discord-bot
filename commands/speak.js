62e36d3886c83478e1702256f2f0a052049cd381
RumiAxolotl <rumiaxolotl@gmail.com>
2025-01-06 19:14:08 +0700

    feat: Add automatic disconnection after audio playback finishes

--- commands/speak.js
+++ commands/speak.js
@@ -40,6 +40,12 @@ module.exports = {
             connection.subscribe(player);
             player.play(resource);
 
+            // Disconnect after audio finishes
+            player.on('stateChange', (oldState, newState) => {
+                if (newState.status === 'idle') {
+                    connection.destroy();
+                }
+            });
 
         } catch (error) {
             console.error(error);
