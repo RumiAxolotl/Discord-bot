303bdec08fd20fa8687597d9ff3d779968bed5f5
RumiAxolotl <rumiaxolotl@gmail.com>
2025-01-12 15:48:33 +0700

    Refactor message handling to improve welcome command execution and streamline speak command conditions

--- events/message.js
+++ events/message.js
@@ -4,13 +4,16 @@ module.exports = {
         // Ignore messages from bots
         if (message.author.bot) return;
 
-    if (message.content.startsWith('wlc')) {
-        const welcomeCommand = client.commands.get('welcome');
-        if (welcomeCommand) {
-            welcomeCommand.run(client, message, message.content.split(' ').slice(1));
+        if (message.content.startsWith('wlc')) {
+            const welcomeCommand = client.commands.get('welcome');
+            if (welcomeCommand) {
+                welcomeCommand.run(client, message, message.content.split(' ').slice(1));
+            }
         }
-        }
-        if (message.member.voice.channel && client.voice.adapters.get(message.guild.id) && message.channel.id == message.guild.me.voice.channelId && !message.content.startsWith(`${process.env.PREFIX}` && !message.content.startsWith('<' ))) {
+        if (message.member.voice.channel &&
+            client.voice.adapters.get(message.guild.id) &&
+            message.channel.id == message.guild.me.voice.channelId &&
+            !message.content.startsWith(process.env.PREFIX)) {
             const speakCommand = client.commands.get('speak');
             if (speakCommand) {
                 speakCommand.run(client, message, [message.content]);
