08eb76c69012855f5034084db967253270a0afce
Phạm Thanh Thiện <rumiaxolotl@gmail.com>
2026-03-21 10:08:15 +0700

    Allow avatar lookup by user ID
    Enhance avatar command to accept a user ID as an argument in addition to mentions. The update strips mention characters from args[0], validates it's numeric, and attempts client.users.fetch(userId); it returns a friendly error if the ID is invalid or the user can't be found. If no valid target is provided, it falls back to the command author.

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,7 +6,24 @@ module.exports = {
 
     async run(client, message, args) {
         if (message.channel.type === "dm") return;
-        let member = message.mentions.users.first() || message.author;
+
+        let member = message.mentions.users.first();
+        if (!member && args[0]) {
+            const userId = args[0].replaceAll(/[<@!>]/g, "");
+            if (/^\d+$/.test(userId)) {
+                try {
+                    member = await client.users.fetch(userId);
+                } catch {
+                    return message.channel.send("I couldn't find a user with that ID.");
+                }
+            } else {
+                return message.channel.send("Please provide a valid user ID or mention a user.");
+            }
+        }
+
+        if (!member) {
+            member = message.author;
+        }
 
         let avatar = member.displayAvatarURL({ dynamic: true, size: 4096 });
 
